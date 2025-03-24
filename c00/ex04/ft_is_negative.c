e3a5703d62fb9a4cadc7052a8f4f1a116641fd3f
Carlos Eduardo Silva Souza De Lima <csilva-s@c2r2p5.42sp.org.br>
2025-03-24 09:25:23 -0300

    finish

---
+++ c00/ex04/ft_is_negative.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 01:51:34 by csilva-s          #+#    #+#             */
+/*   Updated: 2025/03/16 02:18:01 by csilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	positive;
+	char	negative;
+
+	positive = 'P';
+	negative = 'N';
+	if (n < 0)
+		write(1, &negative, 1);
+	else
+		write(1, &positive, 1);
+}
