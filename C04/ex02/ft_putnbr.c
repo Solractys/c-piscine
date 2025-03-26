52858d08b565263e9c13cd7dd794fb0a73a78a2e
Carlos Eduardo Silva Souza De Lima <csilva-s@c2r3p6.42sp.org.br>
2025-03-26 15:18:51 -0300

    C04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 15:44:43 by csilva-s          #+#    #+#             */
+/*   Updated: 2025/03/25 18:26:09 by csilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nb);
+
+void	ft_putchar(char a);
+
+void	ft_putchar(char a)
+{
+	write (1, &a, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+/* #include <stdio.h> */
+/* int	main(void) */
+/* { */
+/* 	int nb = -2147401514; */
+/* 	ft_putnbr(nb); */
+/* 	return (0); */
+/* } */
